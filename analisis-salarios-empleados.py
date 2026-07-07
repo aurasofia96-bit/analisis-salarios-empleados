e478347df9eb1520f24f54f642f7edc723c81029
aurasofia96-bit <aurasofia96@gmail.com>
2026-07-06 20:50:10 -0500

    Feat: Agregar listas y bucles

--- analisis-salarios-empleados.py
+++ analisis-salarios-empleados.py
@@ -2,7 +2,8 @@
 numero_trabajadores=10
 #Variables
 datos = []
-salario_A=salario_B=salario_C=contador_A=contador_B=contador_C=edad_A=edad_B=edad_C=0
+contador=[['A',0,0,0,0,0,0],['B',0,0,0,0,0,0],['C',0,0,0,0,0,0]]
+# [grupo, suma_salarios, cantidad, suma_edades, prom_salario, prom_edad, incremento]
 #Pedido de datos
 for i in range(numero_trabajadores):
     print()
@@ -26,66 +27,45 @@ for i in range(numero_trabajadores):
     #Clasificar por grupos /////////////////////////
     if trabajador[1] <= 2: 
         trabajador.append('A')
-        salario_A+=trabajador[0]
-        contador_A+= 1
-        edad_A+=trabajador[2]
+        contador[0][1] += trabajador[0]
+        contador[0][2] += 1
+        contador[0][3] += trabajador[2]
     elif trabajador[1] > 8:
         trabajador.append('C')
-        salario_C+=trabajador[0]
-        contador_C+= 1
-        edad_C+=trabajador[2]
+        contador[2][1] += trabajador[0]
+        contador[2][2] += 1
+        contador[2][3] += trabajador[2]
     else:
         trabajador.append('B')
-        salario_B+=trabajador[0]
-        contador_B+= 1
-        edad_B+=trabajador[2]
+        contador[1][1] += trabajador[0]
+        contador[1][2] += 1
+        contador[1][3] += trabajador[2]
     datos.append(trabajador)
-print(datos)
 # Calcular promedios
-if contador_A == 0:
-    salario_promedio_A = 0
-    edad_promedio_A = 0
-else:
-    salario_promedio_A=salario_A/contador_A
-    edad_promedio_A=edad_A/contador_A
-if contador_B == 0:
-    salario_promedio_B = 0
-    edad_promedio_B = 0
-else:
-    salario_promedio_B=salario_B/contador_B
-    edad_promedio_B=edad_B/contador_B
-if contador_C == 0:
-    salario_promedio_C = 0
-    edad_promedio_C = 0
-else:
-    salario_promedio_C=salario_C/contador_C
-    edad_promedio_C=edad_C/contador_C
+for i in range(3):
+    if contador[i][2] > 0:
+        contador[i][4] += (contador[i][1]/contador[i][2])
+        contador[i][5] += (contador[i][3]/contador[i][2])
 # Incremento por grupo
-if salario_promedio_A > 2000000:
-    incremento_A=0.04
-else:
-    incremento_A=0.07
-if salario_promedio_B > 2400000:
-    incremento_B=0.05
-else:
-    incremento_B=0.08
-if salario_promedio_C > 3000000:
-    incremento_C=0.06
-else:
-    incremento_C=0.10
+if contador[0][2] > 0:
+    if contador[0][4] > 2000000:
+        contador[0][6] += 0.04
+    else:
+        contador[0][6] += 0.07
+if contador[1][2] > 0:
+    if contador[1][4] > 2400000:
+        contador[1][6] += 0.05
+    else:
+        contador[1][6] += 0.08
+if contador[2][2] > 0:
+    if contador[2][4] > 3000000:
+        contador[2][6] += 0.06
+    else:
+        contador[2][6] += 0.1
 #Mostrar resultados
-print(f'''Grupo A:
-      • Promedio de salarios: {salario_promedio_A}
-      • Suma total de salarios: {salario_A}
-      • Porcentaje de incremento: {int(incremento_A*100)}%
-      • Promedio de edades: {edad_promedio_A}''')
-print(f'''Grupo B:
-      • Promedio de salarios: {salario_promedio_B}
-      • Suma total de salarios: {salario_B}
-      • Porcentaje de incremento: {int(incremento_B*100)}%
-      • Promedio de edades: {edad_promedio_B}''')
-print(f'''Grupo C:
-      • Promedio de salarios: {salario_promedio_C}
-      • Suma total de salarios: {salario_C}
-      • Porcentaje de incremento: {int(incremento_C*100)}%
-      • Promedio de edades: {edad_promedio_C}''')
+for i in range(3):
+    print(f'''Grupo {contador[i][0]}:
+      • Promedio de salarios: {round(contador[i][4])}
+      • Suma total de salarios: {contador[i][1]}
+      • Porcentaje de incremento: {int(contador[i][6]*100)}%
+      • Promedio de edades: {round(contador[i][5])}''')
